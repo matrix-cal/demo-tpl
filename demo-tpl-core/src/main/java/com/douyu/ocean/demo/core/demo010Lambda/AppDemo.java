15fce843e26c45d280fbf1ce3e20ec9470577db6
weiqi <515324060@qq.com>
2018-06-09 23:02:05 +0800

    test git push by idea test

--- demo-tpl-core/src/main/java/com/douyu/ocean/demo/core/demo010Lambda/AppDemo.java
+++ demo-tpl-core/src/main/java/com/douyu/ocean/demo/core/demo010Lambda/AppDemo.java
@@ -46,7 +46,7 @@ public class AppDemo {
         predicate.test(1);
         //predicate.and();
 
-        //# 有一个参数, 无返回值test
+        //# 有一个参数, 无返回值test2
         Consumer<String> consumer = (x) -> {
             System.out.println(x);
         };

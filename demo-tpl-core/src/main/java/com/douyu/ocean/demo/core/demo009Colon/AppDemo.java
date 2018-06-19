1f4d5e227395f161cf8eefed26ff6e71b877ea9d
weiqi <515324060@qq.com>
2018-06-20 00:37:58 +0800

    - complete method ref

--- demo-tpl-core/src/main/java/com/douyu/ocean/demo/core/demo009Colon/AppDemo.java
+++ demo-tpl-core/src/main/java/com/douyu/ocean/demo/core/demo009Colon/AppDemo.java
@@ -1,8 +1,12 @@
 package com.douyu.ocean.demo.core.demo9Colon;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BiPredicate;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 /**
  * Demo000
@@ -12,45 +16,38 @@ import java.util.List;
  */
 public class AppDemo {
 
-
+    /**
+     * 方法引用, 两个冒号; 个人理解是 只有一句话的lambda表达式,且input param与 形参相同; 常用语foreach
+     */
     public static void test01() {
-        List<String> list = Arrays.asList("a", "b", "d");
-        // 极简模式
-        System.out.println("#####极简模式");
-        list.forEach(s -> System.out.println(s));
+        List<String> list = Arrays.asList("a","c", "b", "d");
+        List<Integer> list2 = Arrays.asList(1, -1, -2, 2);
 
-        // 带参数类型模式
-        System.out.println("#####带参数类型模式");
-        list.forEach((String e) -> System.out.println(e));
+        //# 对象引用::实例方法名, 实例上的实例方法引用, instance::method
+        PrintStream out = System.out;
+        list.forEach(out::println);
 
-        // 多条处理语句
-        System.out.println("#####多条处理语句");
-        list.forEach(e -> {
-            System.out.print(e);
-            System.out.println(e);
-        });
 
-        // 隐式转换变量为final
-        int firstWord = 1;
-        System.out.println("#####test");
-        // firstWord = 2;
-        list.forEach(e -> {
-            System.out.println(firstWord + e);
-        });
+        //# 类名::实例方法名, Class::method, 类的实例方法引用, 首先判断是不是静态方法, 不是静态方法, 则把第一个参数作为实例对象,
+        BiPredicate<String, String> b = String::equals;
+        System.out.println(b.test("abc", "abcd"));
 
-        // 自动推断return 返回值
-        System.out.println("#####自动推断return 返回值");
-        list.sort((e1, e2) -> -e1.compareTo(e2));
-        list.forEach(e -> System.out.println(e));
-        System.out.println("#####");
+        //# 类名::静态方法名,静态方法引用, Class::static_method
+        Consumer<Integer> function = Math::abs;
+        list2.forEach(function);
 
+        //# 构造器, 构造方法引用, Class< T >::new
+        //Function<String, StringBuffer> function1 = StringBuffer::new;
+        Function<Integer, Integer> function1 = Integer::new;
+        Integer num = function1.apply(123);
+        System.out.println(num);
     }
 
 
 
 
     public static void main(String[] args) throws IOException {
-//        test01();
+        test01();
 //        test02();
     }
 }

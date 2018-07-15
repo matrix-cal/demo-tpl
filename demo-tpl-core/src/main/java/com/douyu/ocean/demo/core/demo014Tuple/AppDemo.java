a66f69321937a87fc56d4b056baf474f47e57b67
weiqi <weiqi@douyu.tv>
2018-07-15 19:24:43 +0800

    - tuple

---
+++ demo-tpl-core/src/main/java/com/douyu/ocean/demo/core/demo014Tuple/AppDemo.java
@@ -0,0 +1,31 @@
+package com.douyu.ocean.demo.core.demo014Tuple;
+
+import java.io.IOException;
+
+/**
+ * Demo000
+ *
+ * @author weiqi
+ * @create 2018-04-18 8:56:00
+ */
+public class AppDemo {
+
+
+    public static void test01() {
+        //# Guava
+
+
+        //# Apache commons-lang3
+
+        //# Apache commons-collections
+
+        //# others javatuples 和 JTuple
+
+    }
+
+
+
+    public static void main(String[] args) throws IOException {
+        test01();
+    }
+}

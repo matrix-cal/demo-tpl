2a9a6a1dd8c729eeef787c5509613d8fb58541c9
weiqi <515324060@qq.com>
2018-07-16 07:25:03 +0800

    - lambda表达式test+

---
+++ demo-tpl-core/src/main/java/com/douyu/ocean/demo/core/demo015Callable/AppDemo.java
@@ -0,0 +1,46 @@
+package com.douyu.ocean.demo.core.demo015Callable;
+
+import java.util.concurrent.Callable;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Demo000
+ *
+ * @author weiqi
+ * @create 2018-04-18 8:56:00
+ */
+public class AppDemo {
+
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AppDemo.class);
+
+
+    public static void main(String[] args) throws InterruptedException {
+        Callable<String> callable = new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                sleep(3000L);
+                return "sleep 3000L";
+            }
+        };
+        
+    }
+
+    /**
+     * 休眠辅助方法
+     * @param milliSecond
+     */
+    public static void sleep(long milliSecond) {
+        try {
+            if (milliSecond <= 0L) {
+                return;
+            }
+            Thread.sleep(milliSecond);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}

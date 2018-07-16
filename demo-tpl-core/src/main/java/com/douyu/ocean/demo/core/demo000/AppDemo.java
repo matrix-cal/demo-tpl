2c62163e5bbe143558465e1f91e5f4e926f00232
weiqi <weiqi@douyu.tv>
2018-07-16 14:13:22 +0800

    - 简单完成java8相关demo中最后一个fork-join的demo; - 有待深入研究fork-join语法

--- demo-tpl-core/src/main/java/com/douyu/ocean/demo/core/demo000/AppDemo.java
+++ demo-tpl-core/src/main/java/com/douyu/ocean/demo/core/demo000/AppDemo.java
@@ -1,5 +1,8 @@
 package com.douyu.ocean.demo.core.demo000;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Demo000
  *
@@ -8,7 +11,26 @@ package com.douyu.ocean.demo.core.demo000;
  */
 public class AppDemo {
 
-    public static void main(String[] args) {
-        System.out.println("hello demo-tpl");
+    private static final Logger LOGGER = LoggerFactory.getLogger(com.douyu.ocean.demo.core.demo016ForkJoin.AppDemo.class);
+
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+    }
+
+    /**
+     * 休眠辅助方法
+     * @param milliSecond
+     */
+    public static void sleep(long milliSecond) {
+        try {
+            if (milliSecond <= 0L) {
+                return;
+            }
+            Thread.sleep(milliSecond);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }

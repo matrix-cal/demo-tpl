3e50527c156d75003843193cfba2f488be0d76b5
weiqi <515324060@qq.com>
2019-09-10 00:02:12 +0800

    feat(meite):  add meite dir

---
+++ demo-tpl-core/src/main/java/com/matrix/call/meite/core/demo000Test/AppDemo.java
@@ -0,0 +1,16 @@
+package com.matrix.call.meite.core.demo000Test;
+
+/**
+ * Demo000
+ *
+ * @author weiqi
+ * @create 2018-04-18 8:56:00
+ */
+public class AppDemo {
+
+    public static void main(String[] args) {
+        System.out.println("hello demo-tpl");
+
+
+    }
+}
